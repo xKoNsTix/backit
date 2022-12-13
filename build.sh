738482fa109b122a2dd0f7cced9532c79464765c
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2022-12-13 12:43:59 +0100

    added favicon

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 npx sass ./src/sass/style.scss ./dist/style.css
 cp ./src/index.html ./dist/
 
-#cp ./src/favicons/*.* ./dist/
+
 cp -R ./src/images ./dist/
 cp -R ./src/fonts ./dist/fonts
 npx esbuild ./src/js/main.js --bundle --outfile=./dist/main.js  --minify
